0463b890116de80e040668c7368e8a4773d77e70
porterOne <44737594+porterOne@users.noreply.github.com>
2021-10-13 18:16:34 +0800

    web modify

--- src/qqqcloud/cn/Algorithm/AlgorithmTest06.java
+++ src/qqqcloud/cn/Algorithm/AlgorithmTest06.java
@@ -10,8 +10,6 @@ public class AlgorithmTest06 {
 
         System.out.print("version one");
         System.out.print("version two");
-        System.out.print("version master master");
-        System.out.print("version hot-fix");
 
     }
 }
