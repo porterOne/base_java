4eff3269d1d518d9ab14083b80d4f31bbd7918f7
hongkai <791780936@qq.com>
2021-10-13 17:47:05 +0800

    version hot-fix

--- src/qqqcloud/cn/Algorithm/AlgorithmTest06.java
+++ src/qqqcloud/cn/Algorithm/AlgorithmTest06.java
@@ -10,7 +10,7 @@ public class AlgorithmTest06 {
 
         System.out.print("version one");
         System.out.print("version two");
-        System.out.print("version master");
+        System.out.print("version hot-fix");
 
     }
 }
