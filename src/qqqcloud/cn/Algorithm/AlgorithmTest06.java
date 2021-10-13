30d5090f22185cf28440f9cff5b85697b1fd82ea
hongkai <791780936@qq.com>
2021-10-13 11:59:20 +0800

    version2

--- src/qqqcloud/cn/Algorithm/AlgorithmTest06.java
+++ src/qqqcloud/cn/Algorithm/AlgorithmTest06.java
@@ -6,9 +6,9 @@ public class AlgorithmTest06 {
 
 
     public static void main(String[] args) {
-
-
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+
+        System.out.print("version one");
 
     }
 }
