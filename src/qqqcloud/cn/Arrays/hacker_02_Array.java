f0f1ca0ef4c729e3afb9e46e1730230bfdefe375
kai <166841303@qq.com>
2021-12-01 20:31:31 +0800

    添加数组的创建三种方式

---
+++ src/qqqcloud/cn/Arrays/hacker_02_Array.java
@@ -0,0 +1,12 @@
+package qqqcloud.cn.Arrays;
+
+import java.lang.reflect.Array;
+
+public class hacker_02_Array {
+    public static void main(String[] args) {
+        // 创建数组对象的三种方法
+        int[] a={};
+        int[] b=new int[10];
+        int[] c=new int[]{1,2,3};
+    }
+}
